bb7645c0f3a4a0c8714ee906999c74cf9026c995
Matheus Mota Oliveira da Silva <theguim7@gmail.com>
2025-11-16 16:20:34 -0300

    Update BancoDeDados.c

--- Banco de dados com .h/BancoDeDados.c
+++ Banco de dados com .h/BancoDeDados.c
@@ -178,7 +178,7 @@ void ler_info_gerente(InfoGerenteDet *g) {
     if (scanf("%d", &tmp) != 1) { limpar_buffer(); tmp = 2; }
     g->funcao = (tmp >=0 && tmp <=2) ? (enum GerenciaFuncao)tmp : GER_ADMINISTRATIVO;
 
-    printf("Cargo do gerente (0-Gerente,1-Subgerente,2-Comum,3-Jovem Aprendiz): ");
+    printf("Cargo do gerente (0-Gerente-chefe,1-Subgerente,2-Comum,3-Jovem Aprendiz): ");
     if (scanf("%d", &tmp) != 1) { limpar_buffer(); tmp = 2; }
     g->cargo_ger = (tmp >=0 && tmp <=3) ? (enum GerCargo)tmp : GER_COMUM;
 }
@@ -267,7 +267,7 @@ void imprimir_info_papel_terminal(const Funcionario *f) {
             const InfoGerenteDet *g = &f->papel_info.ger;
             const char *funcao_str = (g->funcao == GER_BANCARIO) ? "Bancario" :
                                      (g->funcao == GER_JUDICIARIO) ? "Judiciario" : "Administrativo";
-            const char *cargo_str = (g->cargo_ger == GERENTE_PLENO) ? "Gerente" :
+            const char *cargo_str = (g->cargo_ger == GERENTE_CHEFE) ? "Gerente-chefe" :
                                     (g->cargo_ger == GER_SUBGERENTE) ? "Subgerente" :
                                     (g->cargo_ger == GER_COMUM) ? "Comum" : "Jovem Aprendiz";
             printf("Papel: Gerente (Funcao: %s, Cargo: %s)\n", funcao_str, cargo_str);
@@ -313,7 +313,7 @@ void imprimir_info_papel_arquivo(FILE *dest, const Funcionario *f) {
             const InfoGerenteDet *g = &f->papel_info.ger;
             const char *funcao_str = (g->funcao == GER_BANCARIO) ? "Bancario" :
                                      (g->funcao == GER_JUDICIARIO) ? "Judiciario" : "Administrativo";
-            const char *cargo_str = (g->cargo_ger == GERENTE_PLENO) ? "Gerente" :
+            const char *cargo_str = (g->cargo_ger == GERENTE_CHEFE) ? "Gerente-chefe" :
                                     (g->cargo_ger == GER_SUBGERENTE) ? "Subgerente" :
                                     (g->cargo_ger == GER_COMUM) ? "Comum" : "Jovem Aprendiz";
             fprintf(dest, "Papel: Gerente (Funcao: %s, Cargo: %s)\n", funcao_str, cargo_str);
